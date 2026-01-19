46b4cb464d326709bcdda71b7d48368971d375dd
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-01-19 16:21:03 +0300

    Update agent_registry.py
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- agents/agent_registry.py
+++ agents/agent_registry.py
@@ -1,1 +1,13 @@
+from agents.forensic_agent import ForensicAgent
+from agents.network_agent import NetworkAgent
+from agents.system_agent import SystemAgent
 
+class AgentRegistry:
+
+    @staticmethod
+    def get_all_agents():
+        return [
+            ForensicAgent("ForensicAgent"),
+            NetworkAgent("NetworkAgent"),
+            SystemAgent("SystemAgent")
+        ]
