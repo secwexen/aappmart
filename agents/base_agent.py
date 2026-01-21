1478aa00c67172de48cc6d61351b4a6129453055
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-01-21 14:35:46 +0300

    Update base_agent.py
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -1,8 +1,1 @@
-class BaseAgent:
 
-    def __init__(self, name: str):
-        self.name = name
-
-    def decide(self, state: dict):
-      
-        raise NotImplementedError("decide() must be implemented by subclasses")
