303b702ce3ac5278002c4c899c186dda20fcf33b
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-01-21 11:15:09 +0300

    Update knowledge_graph.py
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- aappmart/core/knowledge_graph.py
+++ aappmart/core/knowledge_graph.py
@@ -1,16 +1,1 @@
-class KnowledgeGraph:
 
-    def __init__(self):
-        self.graph = {}
-
-    def add_node(self, node):
-        if node not in self.graph:
-            self.graph[node] = []
-
-    def add_edge(self, from_node, to_node):
-        self.add_node(from_node)
-        self.add_node(to_node)
-        self.graph[from_node].append(to_node)
-
-    def __repr__(self):
-        return str(self.graph)
